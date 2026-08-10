3a2f98d00928205698ae9caeefbfd70101bd31b5
Harsh <hr239531@gmail.com>
2026-08-10 15:24:27 +0530

    Add transaction_id migration

---
+++ database/migrations/2026_08_10_095347_add_transaction_id_to_event_registrations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('event_registrations', function (Blueprint $table) {
+            if (!Schema::hasColumn('event_registrations', 'transaction_id')) {
+                $table->string('transaction_id')->nullable()->unique()->after('payment_status');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('event_registrations', function (Blueprint $table) {
+            $table->dropColumn('transaction_id');
+        });
+    }
+};
